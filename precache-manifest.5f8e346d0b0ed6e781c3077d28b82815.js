d0b96a268d67c36b29257140d073c357091b93af
ametelica <al420@tut.by>
2019-08-07 13:45:45 +0300

    Updates

---
+++ precache-manifest.5f8e346d0b0ed6e781c3077d28b82815.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6610beb5f3b099799f7ce9e50589b15",
+    "url": "/vk_au_apps/index.html"
+  },
+  {
+    "revision": "a546f10d789d03f81dc2",
+    "url": "/vk_au_apps/static/css/2.618af2ae.chunk.css"
+  },
+  {
+    "revision": "a546f10d789d03f81dc2",
+    "url": "/vk_au_apps/static/js/2.d51dd72f.chunk.js"
+  },
+  {
+    "revision": "1c701b5472aab9b531b0",
+    "url": "/vk_au_apps/static/js/main.e17e2729.chunk.js"
+  },
+  {
+    "revision": "5f535d4bffc2567b9b16",
+    "url": "/vk_au_apps/static/js/runtime~main.abb95ca0.js"
+  }
+]);
